68e86fb4986e2eb47acda300027661f000b44b9e
nicolas sabogal <nicolassabogal85@gmail.com>
2020-11-20 11:19:35 -0500

    start project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,15 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { ImageSearchComponent } from './imageSearch/imageSearch.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [	
+    AppComponent,
+      HeaderComponent,
+      ImageSearchComponent
+   ],
   imports: [
     BrowserModule,
     AppRoutingModule
---
+++ src/app/imageSearch/imageSearch.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-imageSearch',
+  templateUrl: './imageSearch.component.html',
+  styleUrls: ['./imageSearch.component.scss']
+})
+export class ImageSearchComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
